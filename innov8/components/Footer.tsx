0e0b90d3c156c3b8d6706c8bbcc1121cc25cb2ec
Madukaaa <madukakarunathilake2003@gmail.com>
2025-11-25 23:45:39 +0530

    scroll prevented

--- innov8/components/Footer.tsx
+++ innov8/components/Footer.tsx
@@ -2,13 +2,8 @@ export default function Footer() {
   return (
     <footer className="w-full bg-black text-white py-8 px-4 md:px-10 overflow-x-hidden">
       <div className="flex flex-col md:flex-row items-center justify-between max-w-screen-xl mx-auto gap-4">
-
         {/* Logo */}
-        <img
-          src="/images/innov8logo.png"
-          alt="Innov8"
-          className="h-6"
-        />
+        <img src="/images/innov8logo.png" alt="Innov8" className="h-6" />
 
         {/* Links */}
         <div className="flex flex-wrap gap-4 md:gap-10 text-sm justify-center">
