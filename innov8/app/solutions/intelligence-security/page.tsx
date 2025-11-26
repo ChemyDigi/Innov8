145373881c0ec8e4abe04f1347b221ce975a8b4f
Madukaaa <madukakarunathilake2003@gmail.com>
2025-11-26 17:09:53 +0530

    Page done

--- innov8/app/solutions/intelligence-security/page.tsx
+++ innov8/app/solutions/intelligence-security/page.tsx
@@ -4,6 +4,7 @@ import ResourcesSection from "@/components/IntelligenceSecurity/ResourcesSection
 import IntelligenceIntro from "@/components/IntelligenceSecurity/IntelligenceIntro";
 import DetailsSection from "@/components/IntelligenceSecurity/DetailsSection";
 import ConnectSection from "@/components/IntelligenceSecurity/ConnectSection";
+import ExploreSection from "@/components/IntelligenceSecurity/ExploreSection";
 
 export default function Page() {
   return (
@@ -13,6 +14,7 @@ export default function Page() {
       <IntelligenceEbook />
       <DetailsSection />
       <ResourcesSection />
+      <ExploreSection />
       <ConnectSection />
     </div>
   );
